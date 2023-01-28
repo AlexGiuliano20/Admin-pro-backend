bbf55ba22b3954c77deb46100db481a1047010a8
AlexGiuliano20 <alex.giuliano37@gmail.com>
2023-01-28 14:49:27 -0300

    Menu del lado del servidor

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,106 +4,110 @@ const bcrypt = require("bcryptjs");
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/jwt");
 const { googleVerify } = require("../helpers/google-verify");
+const { getMenuFrontEnd } = require("../helpers/menu-frontend");
 
 const login = async (req, res = response) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  try {
-    // Verificar email
-    const usuarioDB = await Usuario.findOne({ email });
+	try {
+		// Verificar email
+		const usuarioDB = await Usuario.findOne({ email });
 
-    if (!usuarioDB) {
-      return res.status(400).json({
-        ok: false,
-        msg: "Email no encontrado",
-      });
-    }
+		if (!usuarioDB) {
+			return res.status(400).json({
+				ok: false,
+				msg: "Email no encontrado",
+			});
+		}
 
-    // Verificar contraseña
-    const validPassword = bcrypt.compareSync(password, usuarioDB.password);
-    if (!validPassword) {
-      return res.status(400).json({
-        ok: false,
-        msg: "Contraseña no válida",
-      });
-    }
+		// Verificar contraseña
+		const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+		if (!validPassword) {
+			return res.status(400).json({
+				ok: false,
+				msg: "Contraseña no válida",
+			});
+		}
 
-    // Generar el TOKEN - JWT
-    const token = await generarJWT(usuarioDB.id);
+		// Generar el TOKEN - JWT
+		const token = await generarJWT(usuarioDB.id);
 
-    res.json({
-      ok: true,
-      token,
-    });
-  } catch (error) {
-    console.log(error),
-      res.status(500).json({
-        ok: false,
-        msg: "Hable con el administrador",
-      });
-  }
+		res.json({
+			ok: true,
+			token,
+			menu: getMenuFrontEnd(usuarioDB.role),
+		});
+	} catch (error) {
+		console.log(error),
+			res.status(500).json({
+				ok: false,
+				msg: "Hable con el administrador",
+			});
+	}
 };
 
 const googleSignIn = async (req, res = response) => {
-  try {
-    const { email, name, picture } = await googleVerify(req.body.token);
+	try {
+		const { email, name, picture } = await googleVerify(req.body.token);
 
-    const usuarioDb = await Usuario.findOne({ email });
-    let usuario;
+		const usuarioDb = await Usuario.findOne({ email });
+		let usuario;
 
-    if (!usuarioDb) {
-      usuario = new Usuario({
-        nombre: name,
-        email,
-        password: "@@@",
-        img: picture,
-        google: true,
-      });
-    } else {
-      usuario = usuarioDb;
-      usuario.google = true;
-    }
+		if (!usuarioDb) {
+			usuario = new Usuario({
+				nombre: name,
+				email,
+				password: "@@@",
+				img: picture,
+				google: true,
+			});
+		} else {
+			usuario = usuarioDb;
+			usuario.google = true;
+		}
 
-    // Guardar usuario
-    await usuario.save();
+		// Guardar usuario
+		await usuario.save();
 
-    // Generar TOKEN - JWT
-    const token = await generarJWT(usuario.id);
+		// Generar TOKEN - JWT
+		const token = await generarJWT(usuario.id);
 
-    res.json({
-      ok: true,
-      email,
-      name,
-      picture,
-      token,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      ok: false,
-      msg: "Token de google no es correcto",
-    });
-  }
+		res.json({
+			ok: true,
+			email,
+			name,
+			picture,
+			token,
+			menu: getMenuFrontEnd(usuario.role),
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({
+			ok: false,
+			msg: "Token de google no es correcto",
+		});
+	}
 };
 
 const renewToken = async (req, res = response) => {
-  const uid = req.uid;
+	const uid = req.uid;
 
-  // Generar el TOKEN - JWT
-  const token = await generarJWT(uid);
+	// Generar el TOKEN - JWT
+	const token = await generarJWT(uid);
 
-  // Obtener usuario por uid
-  const usuario = await Usuario.findById(uid);
+	// Obtener usuario por uid
+	const usuario = await Usuario.findById(uid);
 
-  res.json({
-    ok: true,
-    token,
-    usuario,
-  });
+	res.json({
+		ok: true,
+		token,
+		usuario,
+		menu: getMenuFrontEnd(usuario.role),
+	});
 };
 
 module.exports = {
-  login,
-  googleSignIn,
-  renewToken,
+	login,
+	googleSignIn,
+	renewToken,
 };
