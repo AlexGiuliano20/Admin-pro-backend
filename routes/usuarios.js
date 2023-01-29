903b3f792c92b4b1a0b31225c095b88895af3625
AlexGiuliano20 <alex.giuliano37@gmail.com>
2023-01-29 01:19:51 -0300

    Validar si es el mismo usuario o un admin

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -10,7 +10,11 @@ const {
 	actualizarUsuario,
 	borrarUsuario,
 } = require("../controllers/usuarios");
-const { validarJWT, validarADMIN_ROLE } = require("../middlewares/validar-jwt");
+const {
+	validarJWT,
+	validarADMIN_ROLE,
+	validarADMIN_ROLE_o_MismoUsuario,
+} = require("../middlewares/validar-jwt");
 
 const router = Router();
 
@@ -31,7 +35,7 @@ router.put(
 	"/:id",
 	[
 		validarJWT,
-		validarADMIN_ROLE,
+		validarADMIN_ROLE_o_MismoUsuario,
 		check("nombre", "El nombre es obligatorio").not().isEmpty(),
 		check("email", "El email es obligatorio").isEmail(),
 		check("role", "El role es obligatorio").not().isEmpty(),
